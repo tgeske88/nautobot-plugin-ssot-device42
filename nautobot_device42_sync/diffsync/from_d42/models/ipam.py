5e22c7c62ec6a25b8582b64f326379f5bd0ec34a
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2021-11-02 08:04:28 -0500

    fix: :bug: Correct Exception to be NautobotVLAN instead of Site.
    Fixed the Exception to be caught to be a VLAN, not Site. Also tweaked the logging to add more info for troubleshooting.

--- nautobot_device42_sync/diffsync/from_d42/models/ipam.py
+++ nautobot_device42_sync/diffsync/from_d42/models/ipam.py
@@ -409,11 +409,11 @@ class VLAN(DiffSyncModel):
         The self.diffsync._objects_to_delete dictionary stores all objects for deletion and removes them from Nautobot
         in the correct order. This is used in the Nautobot adapter sync_complete function.
         """
-        self.diffsync.job.log_warning(f"VLAN {self.name} will be deleted.")
+        self.diffsync.job.log_warning(f"VLAN {self.name} {self.vlan_id} {self.building} will be deleted.")
         super().delete()
         try:
             vlan = NautobotVLAN.objects.get(vid=self.vlan_id, name=self.name, site__name=self.building)
-        except NautobotSite.DoesNotExist:
+        except NautobotVLAN.DoesNotExist:
             vlans = NautobotVLAN.objects.filter(vid=self.vlan_id, name=self.name)
             for _vlan in vlans:
                 if not _vlan.site:
