6bbd2c1792c84a5fa72cdcb2c838d6a943bfa8d5
Justin Drew <2396364+jdrew82@users.noreply.github.com>
2021-12-01 20:33:00 +0000

    test: :white_check_mark: Add test for `get_subnet_default_custom_fields` method

--- nautobot_ssot_device42/tests/unit/test_utils_device42.py
+++ nautobot_ssot_device42/tests/unit/test_utils_device42.py
@@ -281,3 +281,18 @@ class TestDevice42Api(TestCase):
         response = self.dev42.get_subnets()
         self.assertEqual(response, expected)
         self.assertTrue(len(responses.calls) == 1)
+
+    @responses.activate
+    def test_get_subnet_default_custom_fields(self):
+        """Test get_subnet_default_custom_fields success."""
+        test_query = load_json("./nautobot_ssot_device42/tests/fixtures/get_subnet_default_custom_fields_sent.json")
+        responses.add(
+            responses.GET,
+            "https://device42.testexample.com/services/data/v1.0/query/?query=SELECT cf.key, cf.value, cf.notes FROM view_subnet_custom_fields_v1 cf&output_type=json&_paging=1&_return_as_object=1&_max_results=1000",
+            json=test_query,
+            status=200,
+        )
+        expected = load_json("./nautobot_ssot_device42/tests/fixtures/get_subnet_default_custom_fields_recv.json")
+        response = self.dev42.get_subnet_default_custom_fields()
+        self.assertEqual(response, expected)
+        self.assertTrue(len(responses.calls) == 1)
